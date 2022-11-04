1b021bbee3e59225aca2f47fbd3d5c70072ddba8
DESKTOP-JQIT7JR\DeDonatisStefano <stefano.dedonatis@linksmt.it>
2022-11-04 17:33:36 +0100

    Aggiunta e implementazione ngxtraslate per italiano inglese e spagnolo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'esameAngular';
+
+  /*constructor(translate: TranslateService) {
+
+    translate.use('en');
+    translate.setDefaultLang('en');
+    
+  }*/
+  constructor(public translate: TranslateService) {
+    translate.setDefaultLang('it');
+
+    const browserLang = translate.getBrowserLang();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CocktailComponent } from './cocktail/cocktail.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RegistrazioneComponent } from './registrazione/registrazione.component';
+import {TranslateModule, TranslateLoader} from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 @NgModule({
   declarations: [
@@ -18,12 +23,20 @@ import { RegistrazioneComponent } from './registrazione/registrazione.component'
     CocktailComponent,
     LoginComponent,
     RegistrazioneComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
