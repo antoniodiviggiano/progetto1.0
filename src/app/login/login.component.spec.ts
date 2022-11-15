906d3c8fe975ddff27e04be870db0236e9cf4bec
Giuseppe Antonio Diviggiano <giuseppe.diviggiano@linksmt.it>
2022-11-15 15:03:45 +0100

    ''

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,23 +1,60 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommonModule } from "@angular/common";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { Component, DebugElement } from "@angular/core";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatIconModule } from "@angular/material/icon";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
-import { LoginComponent } from './login.component';
+import { RouterTestingModule } from "@angular/router/testing";
+import { TranslateFakeLoader, TranslateLoader, TranslateModule, TranslateParser, TranslateService, TranslateStore, USE_DEFAULT_LANG } from "@ngx-translate/core";
+import { TranslateTestingModule } from "ngx-translate-testing";
+import { AppComponent } from "../app.component";
+import { AuthService } from "../auth/auth.service";
+import { RegistrazioneComponent } from "../registrazione/registrazione.component";
+
+import { LoginComponent } from "./login.component";
+/* 
+const loginServiceStub = {
+  logIn(data: { email: string; password: string }) {
+    const user = {
+      password: "123456789",
+      email: "test@test",
+    };
+    return user;
+  },
+};  */
 
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
-
   beforeEach(async () => {
+
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [RegistrazioneComponent],
+      imports: [RouterTestingModule,
+        HttpClientTestingModule,
+        MatIconModule,
+        ReactiveFormsModule,
+        MatToolbarModule,
+        TranslateModule.forRoot(),
+        TranslateTestingModule.withTranslations({ en: require('src/assets/i18n/en-EN.json'), es: require('src/assets/i18n/es-ES.json'), it: require('src/assets/i18n/it-IT.json') })
+      ],
     })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      .compileComponents();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  
+
+  fit('should create the app', () => {
+    const fixture = TestBed.createComponent(LoginComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
+
+  xit('email check - utente non valido', ()=> {
+    
+  })
+
+
+
 });
--- src/app/registrazione/registrazione.component.spec.ts
+++ src/app/registrazione/registrazione.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/* import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegistrazioneComponent } from './registrazione.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 
 describe('RegistrazioneComponent', () => {
   let component: RegistrazioneComponent;
@@ -8,7 +10,9 @@ describe('RegistrazioneComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegistrazioneComponent ]
+      declarations: [ RegistrazioneComponent ],
+      providers: [HttpClientTestingModule]
+
     })
     .compileComponents();
 
@@ -21,3 +25,4 @@ describe('RegistrazioneComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+ */
